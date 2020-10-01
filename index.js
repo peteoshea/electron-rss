e275cf3a00435885f27b22d342ab82a42a64806d
Pete O'Shea <mail@peteoshea.co.uk>
2020-10-01 18:11:26 +0100

    Add Tailwind CSS to the project

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ try {
   require('electron-reloader')(module);
 } catch (_) {}
 
+const fs = require('fs');
+
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
@@ -15,6 +17,15 @@ function createWindow() {
 
   // and load the index.html of the app.
   win.loadFile('index.html');
+
+  win.webContents.on('did-finish-load', function () {
+    fs.readFile(__dirname + '/tailwind-ui.min.css', 'utf-8', function (error, data) {
+      if (!error) {
+        var formatedData = data.replace(/\s{2,10}/g, ' ').trim();
+        win.webContents.insertCSS(formatedData);
+      }
+    });
+  });
 }
 
 app.whenReady().then(createWindow);

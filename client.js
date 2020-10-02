e9313276b55cf41e3b7ebc2cc0fc5b9823572a6b
Pete O'Shea <mail@peteoshea.co.uk>
2020-10-02 17:49:03 +0100

    Store new feed addresses using electron-store

--- client.js
+++ client.js
@@ -1,3 +1,7 @@
+const { ipcMain } = require('electron');
+const Store = require('electron-store');
+const store = new Store();
+
 document.addEventListener('DOMContentLoaded', (event) => {
   document.querySelector('#add-blog').addEventListener('click', () => {
     document.querySelector('#main-screen').classList.add('hidden');
@@ -7,5 +11,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
   document.querySelector('#back').addEventListener('click', () => {
     document.querySelector('#main-screen').classList.remove('hidden');
     document.querySelector('#form').classList.add('hidden');
+  });
+
+  document.querySelector('form').addEventListener('submit', () => {
+    store.set('feeds', [...(store.get('feeds') || []), document.querySelector('#feedurl').value]);
   });
 });
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      enableRemoteModule: true,
     },
   });
 
